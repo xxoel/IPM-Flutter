808652925ed3e5f85daae1654e7b3818b872d5f8
xoel <xoel.diaz@udc.es>
2022-11-17 19:01:33 +0100

    Añadido scroll para orientacion horizontal

--- app_movil/lib/main.dart
+++ app_movil/lib/main.dart
@@ -67,71 +67,82 @@ class _MyHomePageState extends State<MyHomePage> {
           .toList();
     });
   }
+Widget makeSearchBar() {
+  Widget searchBar = Container(
+    margin: const EdgeInsets.only(top: 100, left: 80, right: 80, bottom: 100),
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+            'assets/chef.png',
+            width: 150,
+            height: 150,
+            fit: BoxFit.fill
+        ),
+        Image.asset(
+            'assets/edamam.png',
+            width: 150,
+            height: 150,
+            fit: BoxFit.fill
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+              controller: _text,
+              onChanged: (value) => searchResults(value),
+              onSubmitted: (value) {
+                setState(() {
+                  /*bool conexion = false;
+                        try{
+                          final result = await InternetAddress.lookup('www.google.com');
+                          if(result.isNotEmpty && result[0].rawAddress.isNotEmpty){
+                            conexion = true;
+                          }
+                        }
+                        on SocketException catch (_){}*/
+                  _text.text.isEmpty ? _validate = true : _validate = false;
+                  if (!_validate) {
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context) => const VentanaBusqueda()),);
+                  }
+                  _itemsSuggestions = [];
+                });
+              },
+              decoration: InputDecoration(
+                  isDense: true,
+                  errorText: _validate ? 'EL CAMPO NO PUEDE ESTAR VACIO' : null,
+                  labelText: "Introduzca los datos",
+                  hintText: "Recetas",
+                  prefixIcon: IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                  ),
+                  suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear),
+                      onPressed: () {
+                        _text.clear();
+                        setState(() {});
+                      }),
+                  border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      )
+                  )
+              )
+          ),
+        )
+      ],
+    ),
+  );
+  return searchBar;
+}
 
   @override
   Widget build(BuildContext context) {
-    Widget searchBar = Container(
-      margin: const EdgeInsets.only(top: 100, left: 80, right: 80,bottom: 100),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget> [
-          Image.asset(
-              'assets/chef.png',
-                width: 150,
-                height: 150,
-                fit: BoxFit.fill
-          ),
-          Image.asset(
-              'assets/edamam.png',
-              width: 150,
-              height: 150,
-              fit:BoxFit.fill
-          ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                    controller : _text,
-                    onChanged: (value) => searchResults(value),
-                    onSubmitted: (value) {
-                      setState(() {
-                        _text.text.isEmpty ? _validate = true : _validate = false;
-                        if(!_validate) {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => const VentanaBusqueda()),);
-                        }
-                        _itemsSuggestions=[];
-                      });
-                    },
-                    decoration: InputDecoration(
-                        isDense: true,
-                        errorText: _validate ? 'EL CAMPO NO PUEDE ESTAR VACIO': null,
-                        labelText:"Introduzca los datos",
-                        hintText: "Recetas",
-                        prefixIcon: IconButton(
-                          icon: const Icon(Icons.arrow_back),
-                          onPressed: () {
-                            FocusManager.instance.primaryFocus?.unfocus();
-                          },
-                        ),
-                        suffixIcon: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              _text.clear();
-                              setState(() {
-                              });
-                            }),
-                        border: const OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10.0),
-                            )
-                        )
-                    )
-                ),
-              )
-              ],
-          ),
-      );
     Color color = Theme.of(context).primaryColor;
-
+    Widget searchBar = makeSearchBar();
     Widget buttonSection = Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
@@ -139,13 +150,8 @@ class _MyHomePageState extends State<MyHomePage> {
         _buildButtonColumn(color, Icons.settings, 'OPT',context),
       ],
     );
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -154,26 +160,15 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          child:Expanded(
+            child: SingleChildScrollView(
+              child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-
-          children: [searchBar,Expanded(child: buttonSection)]
+          children: [searchBar,buttonSection]
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    )
+    )
     );
   }
 }
@@ -286,7 +281,8 @@ class VentanaOpciones extends StatelessWidget {
               width: 250,
               height: 250,
               fit: BoxFit.fill),
-            Row(
+            Expanded(
+            child:Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [ButtonBar(
@@ -314,7 +310,7 @@ class VentanaOpciones extends StatelessWidget {
                     )
                   ],
                 ),
-              ])],
+              ]))],
         )
     ));
   }
