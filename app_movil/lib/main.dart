233cd8ee81e62ee2655ed7a7796d6ac2583ea3bf
xoel <xoel.diaz@udc.es>
2022-11-20 21:05:38 +0100

    Añadido error de servidor/red y añadido nombre e imagen a las busquedas

--- app_movil/lib/main.dart
+++ app_movil/lib/main.dart
@@ -1,9 +1,70 @@
 import 'dart:io';
 import 'package:app_movil/edamam.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'dart:async'; //For StreamController/Stream
 
-void main() {
-  runApp(const MyApp());
+
+class ConnectionStatusSingleton {
+
+  static final ConnectionStatusSingleton _singleton = ConnectionStatusSingleton._internal();
+  ConnectionStatusSingleton._internal();
+
+  static ConnectionStatusSingleton getInstance() => _singleton;
+
+  int hasConnection = 0;
+
+  StreamController connectionChangeController =StreamController.broadcast();
+
+  final Connectivity _connectivity =Connectivity();
+
+  void initialize() {
+    _connectivity.onConnectivityChanged.listen(_connectionChange);
+    checkConnection();
+  }
+
+  Stream get connectionChange => connectionChangeController.stream;
+
+  //A clean up method to close our StreamController
+  // Because this is meant to exist through the entire application life cycle this isn't
+  // really an issue
+  void dispose() {
+    connectionChangeController.close();
+  }
+  //flutter_connectivity's listener
+  void _connectionChange(ConnectivityResult result) {
+    checkConnection();
+  }
+  //The test to actually see if there is a connection
+  Future checkConnection() async {
+    int previousConnection = hasConnection;
+    try {
+      final result =await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        try{
+          final result = await InternetAddress.lookup('www.edamam.com');
+          if(result.isNotEmpty && result[0].rawAddress.isNotEmpty){
+            hasConnection = 0; //CONNECTION OK
+          }
+        } on SocketException catch(_) {
+          hasConnection = 1; //SERVER
+        }
+      }
+    } on SocketException catch(_) {
+      hasConnection = 2; //RED
+    }
+    //The connection status changed send out an update to all listeners
+    if (previousConnection != hasConnection) {
+      connectionChangeController.add(hasConnection);
+    }
+    return hasConnection;
+  }
+}
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  ConnectionStatusSingleton connectionStatusSingleton = ConnectionStatusSingleton.getInstance();
+  connectionStatusSingleton.initialize();
+    runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -30,7 +91,27 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+
+
 class _MyHomePageState extends State<MyHomePage> {
+
+  late StreamSubscription connectionChangeStream;
+
+  int isOffline = 0;
+
+  @override
+  initState() {
+    super.initState();
+
+    ConnectionStatusSingleton connectionStatus = ConnectionStatusSingleton.getInstance();
+    connectionChangeStream = connectionStatus.connectionChange.listen(connectionChanged);
+  }
+
+  void connectionChanged(dynamic hasConnection) {
+    setState(() {
+      isOffline = hasConnection;
+    });
+  }
   late Future<RecipeBlock?> recetas;
   final _text = TextEditingController();
   bool _validate = false;
@@ -43,7 +124,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget makeSearchBar() {
     Widget searchBar = Container(
       margin: const EdgeInsets.only(top: 100, left: 80, right: 80, bottom: 100),
-              child: Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image.asset(
@@ -56,12 +137,35 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: const EdgeInsets.all(8.0),
            child: TextField(
                 controller: _text,
+               decoration: InputDecoration(
+                   isDense: true,
+                   errorText: _validate ? 'EL CAMPO NO PUEDE ESTAR VACIO' : null,
+                   labelText: "Introduzca los datos",
+                   hintText: "Recetas",
+                   prefixIcon: IconButton(
+                     icon: const Icon(Icons.arrow_back),
+                     onPressed: () {
+                       FocusManager.instance.primaryFocus?.unfocus();
+                     },
+                   ),
+                   suffixIcon: IconButton(
+                       icon: const Icon(Icons.clear),
+                       onPressed: () {
+                         _text.clear();
+                         setState(() {});
+                       }),
+                   border: const OutlineInputBorder(
+                       borderRadius: BorderRadius.all(
+                         Radius.circular(10.0),
+                       )
+                   )
+               ),
                 onSubmitted: (value) {
-                  _text.text.isEmpty ? _validate = true : _validate = false;
-                  if(!_validate){
-                  setState(() async {
-                    int checkConnection = await _checkConnection();
-                    if (checkConnection == 0) {
+                  setState(()  {
+                    _text.text.isEmpty ? _validate = true : _validate = false;
+                    if(!_validate){
+                    //int checkConnection = await _checkConnection();
+                   // if (checkConnection == 0) {
                         search_recipes(_text.text).then((value) {
                         if (value?.count == 0) {
                           Navigator.push(context, MaterialPageRoute(builder: (
@@ -74,42 +178,9 @@ class _MyHomePageState extends State<MyHomePage> {
                         }
                       },
                       );
-                    }
-                    else {
-                      if (checkConnection == 1) {
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) => const VentanaErrorServer()));
-                      }
-                      if (checkConnection == 2) {
-                        Navigator.push(context, MaterialPageRoute(
-                            builder: (context) => const VentanaErrorRed()));
-                      }
-                    }
-                  });}
+                  }});
+
                },
-                decoration: InputDecoration(
-                    isDense: true,
-                    errorText: _validate ? 'EL CAMPO NO PUEDE ESTAR VACIO' : null,
-                    labelText: "Introduzca los datos",
-                    hintText: "Recetas",
-                    prefixIcon: IconButton(
-                     icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                     },
-                    ),
-                    suffixIcon: IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {
-                          _text.clear();
-                         setState(() {});
-                        }),
-                   border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10.0),
-                       )
-                   )
-               )
             ),
          )
         ],
@@ -137,13 +208,63 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
           child:Expanded(
             child: SingleChildScrollView(
-              child: Column(
+              child: isOffline !=0 ? Column(
+                children: [isOffline == 1 ? _errorServerConection() : _errorRedConection()]
+              ): Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [searchBar,buttonSection]
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     )
     )
+    );
+  }
+
+  Center _errorServerConection(){
+    return Center(
+        child: Column(
+            children: [Image.asset(
+                'assets/PinguinoServer.jpeg',
+                width: 250,
+                height: 250,
+                fit: BoxFit.fill),
+              const TextField(
+                enabled: false,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white,fontSize: 22,height: 2.0),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Ha ocurrido un error de conexión con el servidor o no'
+                        'se ha podido establecer. Espere unos segundos o inténtelo de'
+                        ' nuevo más tarde'
+                ),
+              )
+            ]
+
+        )
+    );
+  }
+
+  Center _errorRedConection(){
+    return Center(
+        child: Column(
+            children: [Image.asset(
+                'assets/PinguinoServer.jpeg',
+                width: 250,
+                height: 250,
+                fit: BoxFit.fill),
+              const TextField(
+                enabled: false,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white,fontSize: 22,height: 2.0),
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Error de red: No se ha podido establecer la conexión a internet.'
+                        ' Compruebe su conexión a la red o inténtelo de nuevo más tarde.'
+                ),
+              )
+            ]
+        )
     );
   }
 }
@@ -173,9 +294,7 @@ class VentanaAyuda extends StatelessWidget {
                     enabled: false,
                     maxLines: 6,
                     minLines: 6,
-
                       style: TextStyle(color:Colors.white,fontSize: 22,height: 2.0),
-
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'En esta pestaña se mostrará como usar la aplicación de búsqueda de recetas.'
@@ -266,90 +385,45 @@ class VentanaOpciones extends StatelessWidget {
   }
 }
 
-class VentanaErrorServer extends StatelessWidget{
-  const VentanaErrorServer ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('VentanaErrorserveS'),
-        ),
-        body: Center(
-            child: Column(
-                children: [Image.asset(
-                    'assets/PinguinoServer.jpeg',
-                    width: 250,
-                    height: 250,
-                    fit: BoxFit.fill),
-                  const TextField(
-                    enabled: false,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white,fontSize: 22,height: 2.0),
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Ha ocurrido un error de conexión con el servidor o no'
-                            'se ha podido establecer. Espere unos segundos o inténtelo de'
-                            ' nuevo más tarde'
-                    ),
-                  )
-                ]
-
-            )
-        )
-    );
-  }
-}
-
-class VentanaErrorRed extends StatelessWidget{
-  const VentanaErrorRed ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('VentanaErrorRed'),
-        ),
-        body: Center(
-            child: Column(
-                children: [Image.asset(
-                    'assets/PinguinoServer.jpeg',
-                    width: 250,
-                    height: 250,
-                    fit: BoxFit.fill),
-                  const TextField(
-                    enabled: false,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white,fontSize: 22,height: 2.0),
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Error de red: No se ha podido establecer la conexión a internet.'
-                            ' Compruebe su conexión a la red o inténtelo de nuevo más tarde.'
-                    ),
-                  )
-                ]
-            )
-        )
-    );
-  }
-}
-
 class VentanaBusqueda extends StatelessWidget {
   final RecipeBlock? block;
   const VentanaBusqueda({super.key, required this.block});
 
   @override
   Widget build(BuildContext context) {
-    for(var recipe in block!.recipes!){
-      print(recipe);
-    }
+    print(search_recipes(block!.nextBlock!));
     return Scaffold(
       appBar: AppBar(
         title: const Text('VentanaBusqueda'),
       ),
       body: Center(
-        child:Column(
-        children:[ ElevatedButton(
+          child:SingleChildScrollView(
+            child:Column(
+              children:[
+                for(var recipe in block!.recipes!)
+                  Ink.image(
+                    width: double.infinity,
+                    height:240,
+                    fit: BoxFit.cover,
+                    image: NetworkImage('${recipe.image}'),
+                    child: Center(
+                        child: Stack(
+                                children:
+                                [Text("${recipe.label}",
+                                   style: TextStyle(
+                                   fontSize: 40,
+                                   foreground: Paint()
+                                     ..style= PaintingStyle.stroke
+                                     ..strokeWidth= 6
+                                     ..color= Colors.black,
+                                 ),),
+                                Text("${recipe.label}",
+                                    style: const TextStyle(
+                                    fontSize: 40,
+                                    color: Colors.white,
+                    )),],),
+            )),
+          ElevatedButton(
           onPressed: () {
             Navigator.pop(context);
 
@@ -363,10 +437,20 @@ class VentanaBusqueda extends StatelessWidget {
             decoration: InputDecoration(
                 border: OutlineInputBorder(),
             ),
-          )
+          ),
+          ElevatedButton(
+            onPressed: () {
+              search_recipes(block!.nextBlock!).then((data){
+                Navigator.pushReplacement(context, MaterialPageRoute(
+                    builder: (context) =>  VentanaBusqueda(block: data)));
+              });
+            },
+            child:const Text('Siguiente página'),
+          ),
       ],
       ),
       )
+    )
     );
   }
 }
@@ -418,26 +502,6 @@ class VentanaBusquedaNoEncontrada extends StatelessWidget {
     )
     );
   }
-}
-
-Future<int> _checkConnection() async{
-  int retorno=0;
-  try{
-    final result = await InternetAddress.lookup('www.edamam.com');
-    if(result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-      retorno = 0; //CONECTADO
-    }
-  } on SocketException catch (_) {
-    try{
-      final result = await InternetAddress.lookup('www.google.com');
-      if(result.isNotEmpty && result[0].rawAddress.isNotEmpty){
-        retorno = 1; //ERROR SERVIDOR
-      }
-    } on SocketException catch (_) {
-      retorno = 2; //ERROR RED
-    }
-  }
-  return retorno;
 }
 
 Column _buildButtonColumn(Color color, IconData icon, String label,BuildContext context) {
