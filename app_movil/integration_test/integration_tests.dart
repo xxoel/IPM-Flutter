ea94cdb6e000434ba351b9f61307e36cbe0b0e7d
xoel <xoel.diaz@udc.es>
2022-12-13 22:00:03 +0100

    Añadido test de busqueda vacia

--- app_movil/integration_test/integration_tests.dart
+++ app_movil/integration_test/integration_tests.dart
@@ -27,13 +27,20 @@ void main(){
       await tester.tap(find.byIcon(Icons.search));
       await tester.pumpAndSettle();
       await tester.testTextInput.receiveAction(TextInputAction.done);
-      await tester.pumpAndSettle(const Duration(seconds: 5));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
       expect(find.byType(Ink), findsAtLeastNWidgets(20));
 
       await tester.tap(find.byKey(infoDetallada));
-      await tester.pumpAndSettle(const Duration(seconds: 3));
+      await tester.pumpAndSettle(const Duration(seconds: 2));
       expect(find.byType(ListView), findsOneWidget);
       expect(find.byType(Container), findsAtLeastNWidgets(7));
+    });
+    testWidgets('Test de busqueda vacia', (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pumpAndSettle(const Duration(seconds: 1));
+      expect(find.text('EL CAMPO NO PUEDE ESTAR VACIO'),findsOneWidget);
     });
   });
   group('Tests de Ventana Ajustes y Ayuda', () {
